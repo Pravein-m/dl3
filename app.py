fdbf1b32d93ee237433676dde0dcf4e6405fca47
Pravein-m <118187277+Pravein-m@users.noreply.github.com>
2023-09-06 02:18:35 +0530

    Update app.py
    Signed-off-by: Pravein-m <118187277+Pravein-m@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,64 +1,54 @@
-import tensorflow
 import streamlit as st
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
-#from utils import predict_label
 from PIL import Image
 import numpy as np
-st.title("Shoe Classification")
-
-st.write("Predict the shoe that is being represented in the image.")
 import urllib.request
 import os
-def load_model():
+
+st.title("Shoe Classification")
+st.write("Predict the shoe that is being represented in the image.")
+
+# Define a function to load the model
+@st.cache(allow_output_mutation=True)
+def load_shoe_model():
     if not os.path.isfile('model.h5'):
         urllib.request.urlretrieve('https://github.com/Pravein-m/dl3/blob/5949d91f1adf00eb97f2d98ff0f1b9d0a0147c62/model.h5', 'model.h5')
-        model=load_model('model.h5')
-    return model
-# model = load_model("model.h5")
-l=['Adidas','Nike']
+    return load_model('model.h5')
 
-# model = load_model("model.h5")
+model = load_shoe_model()
+class_labels = ['Adidas', 'Nike']
 
-uploaded_file = st.file_uploader(
-    "Upload an image of a seed :", type="jpg"
-)
-predictions=-1
+uploaded_file = st.file_uploader("Upload an image of a shoe:", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
     image1 = Image.open(uploaded_file)
-    image1=image.smart_resize(image1,(224,224))
+    image1 = image1.resize((224, 224))  # Resize the image to match the model input size
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
+    img_array = img_array / 255.0
     predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
+    label = class_labels[np.argmax(predictions)]
 
-
-st.write("### Prediction Result")
-if st.button("Predict"):
-    if uploaded_file is not None:
-        image1 = Image.open(uploaded_file)
+    st.write("### Prediction Result")
+    if st.button("Predict"):
         st.image(image1, caption="Uploaded Image", use_column_width=True)
-        st.markdown(
-            f"<h2 style='text-align: center;'>Image of {label}</h2>",
-            unsafe_allow_html=True,
-        )
-    else:
-        st.write("Please upload file or choose sample image.")
+        st.markdown(f"<h2 style='text-align: center;'>Image of {label}</h2>", unsafe_allow_html=True)
+else:
+    st.write("Please upload a file or choose a sample image.")
 
-
-st.write("If you would not like to upload an image, you can use the sample image instead:")
+# Provide an option to use a sample image
 sample_img_choice = st.button("Use Sample Image")
-
 if sample_img_choice:
-    image1 = Image.open("")
-    image1=image.smart_resize(image1,(224,224))
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
-    predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
-    st.markdown(
-        f"<h2 style='text-align: center;'>{label}</h2>",
-        unsafe_allow_html=True,
-    )
+    sample_image_path = "sample_image.jpg"  # Provide the path to your sample image
+    sample_image = Image.open(sample_image_path)
+    sample_image = sample_image.resize((224, 224))
+    sample_img_array = image.img_to_array(sample_image)
+    sample_img_array = np.expand_dims(sample_img_array, axis=0)
+    sample_img_array = sample_img_array / 255.0
+    sample_predictions = model.predict(sample_img_array)
+    sample_label = class_labels[np.argmax(sample_predictions)]
+
+    st.write("### Prediction Result for Sample Image")
+    st.image(sample_image, caption="Sample Image", use_column_width=True)
+    st.markdown(f"<h2 style='text-align: center;'>{sample_label}</h2>", unsafe_allow_html=True)
