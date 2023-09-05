2decb7ef075c705550db811b513126b62511f28d
Pravein-m <118187277+Pravein-m@users.noreply.github.com>
2023-09-06 02:43:01 +0530

    Update app.py
    Signed-off-by: Pravein-m <118187277+Pravein-m@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,56 +1,83 @@
+
 import streamlit as st
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-from PIL import Image
-import numpy as np
-import urllib.request
 import os
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')
+import pathlib
+from keras.models import load_model
+import matplotlib.image as mpimg
 
-st.title("Shoe Classification")
-st.write("Predict the shoe that is being represented in the image.")
 
-# Define a function to load the model
-@st.cache(allow_output_mutation=True)
-def load_shoe_model():
-    # model_path = 'model.h5'  # Update this path to the correct location of your 'model.h5' file
-    if not os.path.isfile('model.h5'):
-        urllib.request.urlretrieve('https://github.com/Pravein-m/dl3/blob/5949d91f1adf00eb97f2d98ff0f1b9d0a0147c62/model.h5', 'model.h5')
-    return load_model('model.h5')
+# Define the home function
+def home():
+    st.write("## Introduction")
+    st.write("This app uses  convolutional neural network  to classify variety rice image into five different class category")
+   
+    
+    st.write("This Data contains around 75k images of size 50x50 distributed under 5 categories.")
+    st.write("'Five' -> 0")
+    st.write("'Zero' -> 1")
+    st.write("'Two' -> 2")
+    st.write("'Four' -> 3")
+    st.write("'One' -> 4")
+    st.write("'Three' -> 5")
 
-# Load the model
-model = load_shoe_model()
-class_labels = ['Adidas', 'Nike']
 
-uploaded_file = st.file_uploader("Upload an image of a shoe:", type=["jpg", "jpeg", "png"])
+# Define the prediction function
+def prediction():
+    
+    st.write("Predict the Rice image that is being represented in the image")
+    
+    # Define the input fields
+    model = load_model("cnn.h5")
 
-if uploaded_file is not None:
-    image1 = Image.open(uploaded_file)
-    image1 = image1.resize((224, 224))  # Resize the image to match the model input size
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0
-    predictions = model.predict(img_array)
-    label = class_labels[np.argmax(predictions)]
 
+    
+    uploaded_file = st.file_uploader(
+        "Upload an image of a Rice Image:", type="jpg"
+    )
+    predictions=-1
+    if uploaded_file is not None:
+        image1 = Image.open(uploaded_file)
+        image1=image.smart_resize(image1,(64,64))
+        img_array = image.img_to_array(image1)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array/255.0
+        predictions = model.predict(img_array)
     st.write("### Prediction Result")
-    if st.button("Predict"):
-        st.image(image1, caption="Uploaded Image", use_column_width=True)
-        st.markdown(f"<h2 style='text-align: center;'>Image of {label}</h2>", unsafe_allow_html=True)
-else:
-    st.write("Please upload a file or choose a sample image.")
+    if st.button("Predict"): 
+        labels = {0:"Five",1:"Zero",2:"Two",3:"Four",4:"One",5:"Three"}
+        if prediction!=-1:
+            if uploaded_file is not None:
+                image1 = Image.open(uploaded_file)
+                st.image(image1, caption="Uploaded Image", use_column_width=True)
+                st.markdown(
+                    f"<h2 style='text-align: center;'>Image of {labels[np.argmax(predictions)]}</h2>",
+                    unsafe_allow_html=True,
+                )
+            else:
+                st.write("Please upload file or choose sample image.")
 
-# Provide an option to use a sample image
-sample_img_choice = st.button("Use Sample Image")
-if sample_img_choice:
-    sample_image_path = "sample_image.jpg"  # Update this with the correct path to your sample image
-    sample_image = Image.open(sample_image_path)
-    sample_image = sample_image.resize((224, 224))
-    sample_img_array = image.img_to_array(sample_image)
-    sample_img_array = np.expand_dims(sample_img_array, axis=0)
-    sample_img_array = sample_img_array / 255.0
-    sample_predictions = model.predict(sample_img_array)
-    sample_label = class_labels[np.argmax(sample_predictions)]
+def main():
+    st.set_page_config(page_title="Rice Image Classification", page_icon=":heart:")
+    st.markdown("<h1 style='text-align: center; color: white;'>Rice Image Classification</h1>", unsafe_allow_html=True)
+# Create the tab layout
+    tabs = ["Home", "Classification"]
+    page = st.sidebar.selectbox("Select a page", tabs)
 
-    st.write("### Prediction Result for Sample Image")
-    st.image(sample_image, caption="Sample Image", use_column_width=True)
-    st.markdown(f"<h2 style='text-align: center;'>{sample_label}</h2>", unsafe_allow_html=True)
+# Show the appropriate page based on the user selection
+    if page == "Home":
+        home()
+    elif page == "Classification":
+        prediction()
+main()
