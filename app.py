715fe6a080fa1128a0bc4604bf44d6a007197105
Pravein-m <118187277+Pravein-m@users.noreply.github.com>
2023-09-06 03:21:14 +0530

    Update app.py
    Signed-off-by: Pravein-m <118187277+Pravein-m@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,60 +1,57 @@
 import streamlit as st
-import os
-import numpy as np
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from utils import predict_label
 from PIL import Image
+import numpy as np
+st.title("Seed Classification")
 
-# Define the prediction function
-def prediction():
-    st.write("Predict the Rice image that is being represented in the image")
-    
-    # Define the input fields
-    model_path = "model.h5"  # Update with the correct path to your model file
-    
-    if not os.path.isfile(model_path):
-        st.write("Model file not found. Please upload the model file or provide the correct path.")
-        return
-    
-    model = load_model(model_path)
-    
-    uploaded_file = st.file_uploader("Upload an image of a Rice Image:", type="jpg")
-    
+st.write("Predict the seed that is being represented in the image.")
+
+model = load_model("model.h5")
+l=['Adidas','Nike']
+
+model = load_model("model.h5")
+
+uploaded_file = st.file_uploader(
+    "Upload an image of a seed :", type="jpg"
+)
+predictions=-1
+if uploaded_file is not None:
+    image1 = Image.open(uploaded_file)
+    image1=image.smart_resize(image1,(224,224))
+    img_array = image.img_to_array(image1)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=l[np.argmax(predictions)]
+
+
+st.write("### Prediction Result")
+if st.button("Predict"):
     if uploaded_file is not None:
         image1 = Image.open(uploaded_file)
-        image1 = image1.resize((64, 64))  # Resize the image to match the model input size
-        img_array = image.img_to_array(image1)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array / 255.0
-        predictions = model.predict(img_array)
-        
-        labels = ["Adidas", "Nike"]
-        
-        st.write("### Prediction Result")
-        if st.button("Predict"): 
-            if uploaded_file is not None:
-                image1 = Image.open(uploaded_file)
-                st.image(image1, caption="Uploaded Image", use_column_width=True)
-                st.markdown(
-                    f"<h2 style='text-align: center;'>Image of {labels[np.argmax(predictions)]}</h2>",
-                    unsafe_allow_html=True,
-                )
-            else:
-                st.write("Please upload file or choose sample image.")
+        st.image(image1, caption="Uploaded Image", use_column_width=True)
+        st.markdown(
+            f"<h2 style='text-align: center;'>Image of {label}</h2>",
+            unsafe_allow_html=True,
+        )
+    else:
+        st.write("Please upload file or choose sample image.")
 
-def main():
-    st.set_page_config(page_title="Rice Image Classification", page_icon=":heart:")
-    st.markdown("<h1 style='text-align: center; color: white;'>Rice Image Classification</h1>", unsafe_allow_html=True)
 
-    # Create the tab layout
-    tabs = ["Home", "Classification"]
-    page = st.sidebar.selectbox("Select a page", tabs)
+st.write("If you would not like to upload an image, you can use the sample image instead:")
+sample_img_choice = st.button("Use Sample Image")
 
-    # Show the appropriate page based on the user selection
-    if page == "Home":
-        home()
-    elif page == "Classification":
-        prediction()
-
-if __name__ == "__main__":
-    main()
+if sample_img_choice:
+    image1 = Image.open("seed_charlock.png")
+    image1=image.smart_resize(image1,(224,224))
+    img_array = image.img_to_array(image1)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=l[np.argmax(predictions)]
+    st.markdown(
+        f"<h2 style='text-align: center;'>{label}</h2>",
+        unsafe_allow_html=True,
+    )
