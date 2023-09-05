81a81bc15867fa6400349a8710c6f0cd30432a96
Pravein-m <118187277+Pravein-m@users.noreply.github.com>
2023-09-06 03:37:27 +0530

    Update app.py
    Signed-off-by: Pravein-m <118187277+Pravein-m@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 import streamlit as st
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-#from utils import predict_label
-from PIL import Image
+import os
+from tensorflow import keras
 import numpy as np
-st.title("Seed Classification")
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')
+import pathlib
+from keras.models import load_model
+import matplotlib.image as mpimg
 
-st.write("Predict the seed that is being represented in the image.")
+st.title("emotion detection")
+
+st.write("Predict the emotion that is being represented in the image.")
 
 model = load_model("model.h5")
-l=['Adidas','Nike']
-
-model = load_model("model.h5")
-
+l=[ 'Adidas','Nike']
 uploaded_file = st.file_uploader(
-    "Upload an image of a seed :", type="jpg"
+    "Upload an image of a emotion :", type="jpg"
 )
 predictions=-1
 if uploaded_file is not None:
     image1 = Image.open(uploaded_file)
-    image1=image.smart_resize(image1,(224,224))
+    image1=image.smart_resize(image1,(112,112))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array/255.0
@@ -45,7 +54,7 @@ sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
     image1 = Image.open("seed_charlock.png")
-    image1=image.smart_resize(image1,(224,224))
+    image1=image.smart_resize(image1,(256,256))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array/255.0
