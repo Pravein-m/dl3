9bc6673bd1ab3b86cbd81bb98a1ed12cc5706078
Pravein-m <118187277+Pravein-m@users.noreply.github.com>
2023-09-06 02:03:49 +0530

    Update app.py
    Signed-off-by: Pravein-m <118187277+Pravein-m@users.noreply.github.com>

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.title("Shoe Classification")
 
 st.write("Predict the shoe that is being represented in the image.")
 import urllib.request
+import os
 def load_model():
     if not os.path.isfile('model.h5'):
         urllib.request.urlretrieve('https://github.com/Pravein-m/dl3/blob/5949d91f1adf00eb97f2d98ff0f1b9d0a0147c62/model.h5', 'model.h5')
